a0ee2c3b3934830da1a1dfc9b121598165264af0
Pedro Castelo Branco Lourenco <pedro.lourenco@gft.com>
2019-05-06 14:58:42 +0200

    Adding V1 of Authentication flow using oAuth and IdentityServer

---
+++ IdentityServer/Store/Clients.cs
@@ -0,0 +1,45 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityModel.OidcConstants;
+
+namespace IdentityServer.Store
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client> {
+            new Client {
+                ClientId = "apiClient",
+                ClientName = "Micro Service Web API",
+                AllowedGrantTypes = IdentityServer4.Models.GrantTypes.ClientCredentials,
+                ClientSecrets = new List<Secret> {
+                    new Secret("superSecretPassword".Sha256())},
+                AllowedScopes = new List<string> {"API.read"}
+            },
+            new Client {
+                ClientId = "openIdConnectClient",
+                ClientName = "Example Implicit Client Application",
+                AllowedGrantTypes = IdentityServer4.Models.GrantTypes.Implicit,
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "role",
+                    "API.write"
+                },
+               // AllowedCorsOrigins = new[] {"http://localhost:32779"},
+                RedirectUris = new List<string> {"http://localhost:2020/signin-oidc"},
+                PostLogoutRedirectUris = new List<string> { "http://localhost:2020/" }
+                //RedirectUris = new List<string> {"http://localhost:6060/signin-oidc"},
+                //PostLogoutRedirectUris = new List<string> { "http://localhost:6060/" }
+            }
+        };
+        }
+    }
+}
---
+++ IdentityServer/Store/Resources.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Store
+{
+    internal class Resources
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource> {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email(),
+            new IdentityResource {
+                Name = "apiUser",
+                UserClaims = new List<string> { "apiUser" }
+            }
+        };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> {
+            new ApiResource {
+                Name = "API",
+                DisplayName = "API",
+                Description = "API Access",
+                UserClaims = new List<string> {"apiUser"},
+                ApiSecrets = new List<Secret> {new Secret("scopeSecret".Sha256())},
+                Scopes = new List<Scope> {
+                    new Scope("API.read"),
+                    new Scope("API.write")
+                }
+            }
+        };
+        }
+    }
+}
